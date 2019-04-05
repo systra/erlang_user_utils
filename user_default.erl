073ebc792135de5e3268439a02273dd0af8f871d
Andrzej Trawiński <andrzej.trawinski@jtendo.com>
2019-04-05 12:56:23 +0200

    Add docsh functions

--- user_default.erl
+++ user_default.erl
@@ -10,6 +10,8 @@
          p/1, dmfa/3,
          l/0, nl/0, mm/0,
          mk/0,
+         h/1, h/2, h/3, s/1, s/2, s/3,
+         t/1, t/2, t/3,
          decompile/1, reloader/0,
          hexstr2bin/1, bin2hexstr/1,
          tc/2, tc/4, tca/2, tca/4]).
@@ -497,3 +499,15 @@ mkint(C) when $A =< C, C =< $F ->
     C - $A + 10;
 mkint(C) when $a =< C, C =< $f ->
     C - $a + 10.
+
+h(ModOrFun) -> docsh_erl:h(ModOrFun).
+h(M, F)     -> docsh_erl:h(M, F).
+h(M, F, A)  -> docsh_erl:h(M, F, A).
+
+s(Fun)      -> docsh_erl:s(Fun).
+s(M, F)     -> docsh_erl:s(M, F).
+s(M, F, A)  -> docsh_erl:s(M, F, A).
+
+t(M)        -> docsh_erl:t(M).
+t(M, T)     -> docsh_erl:t(M, T).
+t(M, T, A)  -> docsh_erl:t(M, T, A).
